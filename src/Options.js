223a60b3cc58f8708a0f86fbcd36f700fd1c750d
poornesh-pH <poornesh9626@gmail.com>
2021-05-17 22:10:36 +0530

    3 files modified, 1 added

--- src/Options.js
+++ src/Options.js
@@ -3,14 +3,17 @@ import Option from './Option';
 
 const Options = props => (
   <div>
-    <h3>Your Items</h3>
-    <button
-      className="button button--link"
-      onClick={props.clearOptions}
-      disabled={!(props.options && props.options.length) > 0}
-    >
-      Remove all
-    </button>
+    <div className="widget-header">
+      <h3 className="widget-title">Your Items</h3>
+      <button
+        className="button button--link"
+        onClick={props.clearOptions}
+        disabled={!(props.options && props.options.length) > 0}
+      >
+        Remove all
+      </button>
+    </div>
+
     {props.options.length > 0 ? (
       <ol>
         {props.options &&
