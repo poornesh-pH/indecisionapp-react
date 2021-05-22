20cfcec3451c3b80ccede4550628d48c922b4c43
poornesh-pH <poornesh9626@gmail.com>
2021-05-22 19:02:28 +0530

    2 files modified

--- src/Action.js
+++ src/Action.js
@@ -2,10 +2,13 @@ import React from 'react';
 
 const Action = props => (
   <div>
-    <button className="big-button" onClick={props.handlePick} disabled={!props.hasOptions}>
+    <button
+      className="big-button"
+      onClick={props.handlePick}
+      disabled={!props.hasOptions}
+    >
       What should I do?
     </button>
-    <br />
     <br />
   </div>
 );
